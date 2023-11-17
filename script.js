78222ee5c189e1ce786cc7d74cc8e0241c3a139a
Leul Mekonnen <lo.ow8120@gmail.com>
2023-11-17 11:40:47 +0300

    Qrcode

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ let Qrtext = document.getElementById("Qrtext");
 
 
 function Generateqr() {
-    Qrimag.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + Qrtext.value;
+    if(Qrtext.value.length > 0){
+        Qrimag.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + Qrtext.value;
     imagebox.classList.add("show-image")
+    }else{
+        Qrtext.classList.add('error');
+        setTimeout(()=>{
+            Qrtext.classList.remove('error');
+        },1000);
+    }
+    
 
 }
